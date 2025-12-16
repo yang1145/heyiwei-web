a3fee28c617f12443473c016e8fb74ad7511c48b
Nico6719 <opqstu1333@outlook.com>
2025-12-16 18:26:09 +0800

    Fix SVG closing tag and adjust QQ group link
    Signed-off-by: Nico6719 <opqstu1333@outlook.com>

--- src/views/About.vue
+++ src/views/About.vue
@@ -107,9 +107,9 @@
                 <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                 <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
               </p>
-                <a href="https://qm.qq.com/q/hBcOiafEti" target="_blank" rel="noopener noreferrer" class="text-decoration-none text-white-50">
+            </svg>
+            <a href="https://qm.qq.com/q/hBcOiafEti" target="_blank" rel="noopener noreferrer" class="text-decoration-none text-white-50">
                官方QQ群
-              </svg>
             </a>
           </div>
           </div>
